58f59276915584ab23953d7fbf5e81d5d3a0d0d1
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-12 19:34:22 +0530

    Add circle area program

---
+++ Easy C++ Tasks/calculate-circle-area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+const float pi=3.14159;
+float r;
+
+int main(){
+
+	cout << "Enter Radius Value: ";
+	cin >> r ;
+	float area = pi * r * r ;
+	cout << "Area is = " << area ;	
+} 
+
