7163ecf4bb5f7d85ad68789a8ddc22e5c8a78302
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-14 13:52:53 +0530

    Added Comments to txt file writer.cpp for better Understanding

--- Easy C++ Tasks/txt File Writer.cpp
+++ Easy C++ Tasks/txt File Writer.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
-#include<fstream>
+#include<iostream>  // Include input-output stream library for console operations
+#include<fstream>   // Include file stream library for reading/writing files
 
 using namespace std;
 
 int main(){
+	// Create an output file stream object named 'outfile'
 	ofstream outfile;
+	
+    // Open (or create if it doesn't exist) a file named "Summary.txt" in write mode
 	outfile.open("Summary.txt",ios_base::out);
+	
+	// Write a heading to the file
 	outfile<<"Here is Your Summary: "<<endl;
+	
+	 // Write a message to the file
 	outfile<<"Everything Is ok!!"<<endl;
+	
+	 // Close the file to save changes and free resources
 	outfile.close();
+	
+	return 0; // Return 0 to indicate successful execution
 }
