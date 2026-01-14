7c28a78dd769ca70c35e683d4e3ffd4dd427e127
Sasindu Sachintha <sasindusachintha1234@gmail.com>
2026-01-14 22:07:25 +0530

    Added comments for cpp loop practice.cpp

--- Easy C++ Tasks/cpp-loop-patterns.cpp
+++ Easy C++ Tasks/cpp-loop-patterns.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 
 int main(){
+	// Outer loop: controls the number of rows (4 rows)
 	for(int i=1;i<5;i++){
+		
+		// Inner loop: prints numbers starting from current row number
+        // and prints 4 numbers in each row
 	for(int j=i;j<i+4;j++){
-		cout<<j;
+		cout<<j;  // print the current number
 	}	
-	cout<<endl;
+	cout<<endl;   // move to the next row after inner loop finishes
+   }
 }
-}
+
+ // Note: Output will be like:
+    // 1234
+    // 2345
+    // 3456
+    // 4567
